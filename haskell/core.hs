524c6c7442f538ed8db7414a44ea1a1b1d5a6f5a
Edward Z. Yang <ezyang@mit.edu>
2013-06-02 14:42:59 -0700

    Add polarity support and restructure.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- haskell/core.hs
+++ haskell/core.hs
@@ -20,38 +20,22 @@ import qualified Text.PrettyPrint as PP
 import Text.PrettyPrint (Doc, (<+>), (<>))
 import Data.String
 
-class Pretty p where
-    ppr :: (Applicative m, LFresh m) => Int -> p -> m Doc
+-- * Implementation of polarity is as described in Martin Steffen's
+--   thesis, but with subtyping omitted (we use the stratified kind
+--   system.)
+-- * Typing rules for inductive data types were lifted from PFPL, but
+--   without the strict positivity requirement.
 
--- warning: Ord is so that it is mappable; but this has LATTICE structure
--- Unspecified > Positive > Constant
---             > Negative >
+-- DATA TYPES
+
+-- Bob Harper suggests that one should distinguish from a data type
+-- and the functor associated with it
 data Polarity = Unspecified | Positive | Negative | Constant
-    deriving (Show, Eq)
-
-data Kind = KType
-          | KArr {- Polarity -} Kind Kind
     deriving (Show)
 
-var :: String -> Expr
-var = Var . string2Name
-
-tvar :: String -> Type
-tvar = TVar . string2Name
-
-instance Pretty Kind where
-    ppr _ KType = return (PP.text "★")
-    ppr p (KArr k1 k2) = prettyParen (p > 0) <$> (parr <$> ppr 0 k1 <*> ppr 1 k2)
-
-pprint = runLFreshM . ppr 0
-parr a b = a <+> PP.text "→" <+> b
-prettyParen True = PP.parens
-prettyParen False = id
-pbind b n k e = PP.text b <> PP.parens (PP.text (show n) <+> PP.colon <+> k) <> PP.text "." <+> e
-
--- mu/fold/roll come from the Haskell tradition
--- PFPL calls these ind/rec/fold
--- Coq calls these Inductive/fix/_
+data Kind = KType
+          | KArr Polarity Kind Kind
+    deriving (Show)
 
 data Type = TVar (Name Type)
           | TArr Type Type
@@ -60,20 +44,6 @@ data Type = TVar (Name Type)
           | TMu Type
           | TForall Type
     deriving (Show)
-
-pbinds p c k b = fmap (prettyParen (p > 0)) . lunbind b $ \(n,t) ->
-    pbind c n <$> ppr 0 k <*> ppr 0 t
-pbinder p c (TLambda k b) = pbinds p c k b
-pbinder p c t = prettyParen (p > 0) <$> ((PP.text c <+>) <$> ppr 1 t)
-papp p t1 t2 = prettyParen (p > 1) <$> ((<+>) <$> ppr 1 t1 <*> ppr 2 t2)
-
-instance Pretty Type where
-    ppr _ (TVar n) = return (PP.text (show n))
-    ppr p (TArr t1 t2) = prettyParen (p > 0) <$> (parr <$> ppr 1 t1 <*> ppr 0 t2)
-    ppr p (TForall t) = pbinder p "∀" t
-    ppr p (TMu t) = pbinder p "μ" t
-    ppr p (TLambda k b) = pbinds p "Λ" k b
-    ppr p (TApp t1 t2) = papp p t1 t2
 
 data Expr = Var (Name Expr)
           | Lambda Type (Bind (Name Expr) Expr)
@@ -84,47 +54,75 @@ data Expr = Var (Name Expr)
           | Roll Type Expr
     deriving (Show)
 
-instance Pretty Expr where
-    ppr _ (Var n) = return (PP.text (show n))
-    ppr p (Lambda t b) = pbinds p "λ" t b
-    ppr p (App e1 e2) = papp p e1 e2
-    ppr p (TyLambda k b) = pbinds p "Λ" k b
-    ppr p (TyApp e t) = papp p e t
-    ppr p (Fold e1 e2) = ppr p (App (App (var "fold") e1) e2)
-    ppr p (Roll t e) = ppr p (App (TyApp (var "roll") t) e)
+-- UNBOUND NONSENSE
 
-$(derive [''Type, ''Expr, ''Kind])
+$(derive [''Type, ''Expr, ''Kind, ''Polarity])
+
 instance Alpha Kind
 instance Alpha Type
 instance Alpha Expr
+instance Alpha Polarity
+
 instance Subst Expr Expr where
     isvar (Var v) = Just (SubstName v)
     isvar _ = Nothing
-instance Subst Expr Type where
-instance Subst Expr Kind where
 instance Subst Type Type where
     isvar (TVar v) = Just (SubstName v)
     isvar _ = Nothing
+instance Subst Expr Type where
+instance Subst Expr Kind where
+instance Subst Expr Polarity where
 instance Subst Type Kind where
+instance Subst Type Polarity where
+
+-- POLARITY IS A LATTICE
+
+lub Unspecified _ = Unspecified
+lub _ Unspecified = Unspecified
+lub Positive Negative = Unspecified
+lub Positive _ = Positive
+lub Negative Positive = Unspecified
+lub Negative _ = Negative
+lub Constant p = p
+
+instance Num Polarity where
+    negate Positive = Negative
+    negate Negative = Positive
+    negate p = p
+
+    Constant * _ = Constant
+    _ * Constant = Constant
+    Unspecified * _ = Unspecified
+    _ * Unspecified = Unspecified
+    Positive * Positive = Positive
+    Positive * Negative = Negative
+    Negative * Negative = Positive
+    Negative * Positive = Negative
+
+    fromInteger = error "fromInteger not defined on polarity"
+    (+) = error "addition not defined on polarity"
+    abs = error "abs not defined on polarity"
+    signum = error "signum not defined on polarity"
+
+-- MONAD PLUMBING
 
 done :: MonadPlus m => m a
 done = mzero
 
-mpbind f b = do
-    (x, t) <- unbind b
-    t' <- f t
-    return (bind x t')
-
 type M = FreshM
 
--- Note: looks INSIDE binders! (might be kind of inefficient)
+-- EVALUATION AND TYPE CHECKING
+
+-- Note: looks INSIDE binders!
 beta :: Type -> MaybeT M Type
 beta (TVar _) = done
 beta (TArr t1 t2) =
         TArr <$> beta t1 <*> pure t2
     <|> TArr <$> pure t1 <*> beta t2
-beta (TLambda k b) =
-        TLambda k <$> mpbind beta b
+beta (TLambda k b) = do
+        (x, t) <- unbind b
+        t' <- beta t
+        return (TLambda k (bind x t'))
 beta (TApp (TLambda _ b) t2) = do
         (n, t1) <- unbind b
         return $ subst n t2 t1
@@ -134,6 +132,8 @@ beta (TApp t1 t2) =
 beta (TMu t) = TMu <$> beta t
 beta (TForall t) = TForall <$> beta t
 
+-- More efficient technique is "normalization by evaluation"
+--   described at http://math.andrej.com/2012/11/11/how-to-implement-dependent-type-theory-ii/
 normalize :: Type -> M Type
 normalize t = do
     m <- runMaybeT (beta t)
@@ -141,11 +141,33 @@ normalize t = do
         Just t' -> normalize t'
         Nothing -> return t
 
+polarityOf :: Map (Name Type) Kind -> Name Type -> Type -> M Polarity
+polarityOf _ x (TVar n) | n == x    = return Positive
+                        | otherwise = return Constant
+polarityOf m x (TArr t1 t2) = do
+    p1 <- polarityOf m x t1
+    p2 <- polarityOf m x t2
+    return (lub (-p1) p2)
+polarityOf m x (TLambda k b) = do
+    (n, t) <- unbind b
+    polarityOf (Map.insert n k m) x t
+polarityOf m x (TApp t1 t2) = do
+    k <- kindOf m t1
+    p1 <- polarityOf m x t1
+    p2 <- polarityOf m x t2
+    case k of
+        KArr p _ _ -> return (lub p1 (p * p2))
+        _ -> error "polarityOf: ill-kinded type application"
+polarityOf m x (TMu t) = polarityOf m x t -- I hope this is right!
+polarityOf m x (TForall t) = polarityOf m x t
+
 kindOf :: Map (Name Type) Kind -> Type -> M Kind
 kindOf m (TVar n) = maybe (error "kindOf: unbound variable") return (Map.lookup n m)
 kindOf m (TLambda k p) = do
     (n, t) <- unbind p
-    KArr k <$> kindOf (Map.insert n k m) t
+    let m' = Map.insert n k m
+    p <- polarityOf m' n t
+    KArr p k <$> kindOf m' t
 kindOf m (TArr t1 t2) = do
     k1 <- kindOf m t1
     unless (k1 `aeq` KType) $ error "kindOf: ill-kinded left argument of type arrow"
@@ -156,19 +178,20 @@ kindOf m (TApp t1 t2) = do
     k1 <- kindOf m t1
     k2 <- kindOf m t2
     case k1 of
-        KArr k11 k12 | k11 `aeq` k2 -> return k12
-                     | otherwise -> error ("kindOf: could not unify " ++ show k11 ++ " and " ++ show k2)
+        KArr _ k11 k12 | k11 `aeq` k2 -> return k12
+                       | otherwise -> error ("kindOf: could not unify " ++ show k11 ++ " and " ++ show k2)
         _ -> error "kindOf: attempt to apply to non-arrow kind"
 kindOf m (TMu t) = do
     k <- kindOf m t
     case k of
-        KArr k1 k2 | k1 `aeq` k2 -> return k1 -- perhaps we shouldn't be polymorphic
-                   | otherwise -> error "kindOf: cannot take fixpoint of non-endomorphic operator"
+        KArr Positive k1 k2 | k1 `aeq` k2 -> return k1 -- perhaps we shouldn't be polymorphic
+                            | otherwise -> error "kindOf: cannot take fixpoint of non-endomorphic operator"
+        KArr _ _ _ -> error "kindOf: cannot take fixpoint of non-positive operator"
         _ -> error "kindOf: cannot take fixpoint of *"
 kindOf m (TForall t) = do
     k <- kindOf m t
     case k of
-        KArr _ k2 -> return k2
+        KArr _ _ k2 -> return k2
         _ -> error "kindOf: cannot universally quantify over *"
 
 kof = runFreshM . kindOf Map.empty
@@ -190,7 +213,7 @@ typeOf' km m (Lambda t b) = do
 typeOf' km m (TyLambda k b) = do
     (n, e) <- unbind b
     t <- typeOf (Map.insert n k km) m e
-    return (TForall (TLambda k (bind n t)))
+    return (TForall (TLambda k (bind n t))) -- note the forall!
 typeOf' km m (App e1 e2) = do
     t1 <- typeOf km m e1
     t2 <- typeOf km m e2
@@ -223,14 +246,75 @@ typeOf' km m (Roll t e) = do
     unless (ta `aeq` ta') $ error "typeOf: cannot unify for roll"
     return (TMu t)
 
-lam :: Type -> String -> Expr -> Expr
+-- PRETTY PRINTING
+
+class Pretty p where
+    ppr :: (Applicative m, LFresh m) => Int -> p -> m Doc
+
+pprint = runLFreshM . ppr 0
+
+instance Pretty Polarity where
+    ppr _ Positive = return $ PP.text "⁺"
+    ppr _ Negative = return $ PP.text "⁻"
+    ppr _ Constant = return $ PP.text "°"
+    ppr _ Unspecified = return $ PP.text "±"
+
+instance Pretty Kind where
+    ppr _ KType = return (PP.text "★")
+    ppr p (KArr po k1 k2) = prettyParen (p > 0) <$> (parr' <$> ppr 0 k1 <*> ppr 0 po <*> ppr 1 k2)
+
+instance Pretty Type where
+    ppr _ (TVar n) = return (PP.text (show n))
+    ppr p (TArr t1 t2) = prettyParen (p > 0) <$> (parr <$> ppr 1 t1 <*> ppr 0 t2)
+    ppr p (TForall t) = pbinder p "∀" t
+    ppr p (TMu t) = pbinder p "μ" t
+    ppr p (TLambda k b) = pbinds p "Λ" k b
+    ppr p (TApp t1 t2) = papp p t1 t2
+
+instance Pretty Expr where
+    ppr _ (Var n) = return (PP.text (show n))
+    ppr p (Lambda t b) = pbinds p "λ" t b
+    ppr p (App e1 e2) = papp p e1 e2
+    ppr p (TyLambda k b) = pbinds p "Λ" k b
+    ppr p (TyApp e t) = papp p e t
+    ppr p (Fold e1 e2) = ppr p (App (App (var "fold") e1) e2)
+    ppr p (Roll t e) = ppr p (App (TyApp (var "roll") t) e)
+
+parr' a b c = a <+> PP.text "→" <> b <+> c
+parr a b = a <+> PP.text "→" <+> b
+prettyParen True = PP.parens
+prettyParen False = id
+pbind b n k e = PP.text b <> PP.parens (PP.text (show n) <+> PP.colon <+> k) <> PP.text "." <+> e
+pbinds p c k b = fmap (prettyParen (p > 0)) . lunbind b $ \(n,t) ->
+    pbind c n <$> ppr 0 k <*> ppr 0 t
+pbinder p c (TLambda k b) = pbinds p c k b
+pbinder p c t = prettyParen (p > 0) <$> ((PP.text c <+>) <$> ppr 1 t)
+papp p t1 t2 = prettyParen (p > 1) <$> ((<+>) <$> ppr 1 t1 <*> ppr 2 t2)
+
+-- EXAMPLE SUPPORT CODE
+
+infixl 9 #, &
+infixr 1 ~>
+
+(&) = TyApp
 lam t x e = Lambda t $ bind (string2Name x) e
-
-tlam :: Kind -> String -> Expr -> Expr
 tlam k x e = TyLambda k $ bind (string2Name x) e
-
-tylam :: Kind -> String -> Type -> Type
 tylam k x t = TLambda k $ bind (string2Name x) t
+var = Var . string2Name
+tvar = TVar . string2Name
+
+class SynApp a where (#) :: a -> a -> a
+instance SynApp Expr where (#) = App
+instance SynApp Type where (#) = TApp
+
+class SynArr a where (~>) :: a -> a -> a
+instance SynArr Type where (~>) = TArr
+instance SynArr Kind where (~>) = KArr Positive -- a "reasonable" default
+
+instance IsString Expr where fromString = var
+instance IsString Type where fromString = tvar
+
+-- EXAMPLES
 
 exIdentity = tlam KType "a"
            . lam "a" "x"
@@ -247,18 +331,4 @@ exRoll = tlam (KType ~> KType) "natf"
        . lam (TForall (tylam KType "n" ("n" ~> "natf" # "n"))) "succ"
        $ Roll "natf" ("succ" & TMu "natf" # "n")
 
-infixl 9 #, &
-infixr 1 ~>
 
-(&) = TyApp
-
-class SynApp a where (#) :: a -> a -> a
-instance SynApp Expr where (#) = App
-instance SynApp Type where (#) = TApp
-
-class SynArr a where (~>) :: a -> a -> a
-instance SynArr Type where (~>) = TArr
-instance SynArr Kind where (~>) = KArr
-
-instance IsString Expr where fromString = var
-instance IsString Type where fromString = tvar
