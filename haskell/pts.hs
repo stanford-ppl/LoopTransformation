dfbb599d5e849000b8b081ec6d5a6f97ac3e4332
Edward Z. Yang <ezyang@mit.edu>
2013-06-04 17:26:58 -0700

    PTS polarity
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- haskell/pts.hs
+++ haskell/pts.hs
@@ -23,6 +23,9 @@ import qualified Text.PrettyPrint as PP
 import Text.PrettyPrint (Doc, (<+>), (<>))
 import Data.String
 
+data Polarity = Unspecified | Positive | Negative | Fixed
+    deriving (Show, Eq)
+
 data Constant = Star
               | StarStar
               | Box
@@ -33,7 +36,7 @@ data Expr = Constant Constant
           | Var (Name Expr)
           | App Expr Expr
           | Lambda Expr (Bind (Name Expr) Expr)
-          | Pi Expr (Bind (Name Expr) Expr)
+          | Pi Polarity Expr (Bind (Name Expr) Expr)
     deriving (Show)
 
 -- notice that Pi has no computational content!
@@ -42,18 +45,47 @@ data UExpr = UVar (Name UExpr)
            | ULambda (Bind (Name UExpr) UExpr)
     deriving (Show)
 
-$(derive [''Expr, ''Constant])
+$(derive [''Expr, ''Constant, ''Polarity])
 $(derive [''UExpr])
 instance Alpha Expr
 instance Alpha Constant
 instance Alpha UExpr
+instance Alpha Polarity
 instance Subst Expr Constant where
+instance Subst Expr Polarity where
 instance Subst Expr Expr where
     isvar (Var v) = Just (SubstName v)
     isvar _ = Nothing
 instance Subst UExpr UExpr where
     isvar (UVar v) = Just (SubstName v)
     isvar _ = Nothing
+
+lub Unspecified _ = Unspecified
+lub _ Unspecified = Unspecified
+lub Positive Negative = Unspecified
+lub Positive _ = Positive
+lub Negative Positive = Unspecified
+lub Negative _ = Negative
+lub Fixed p = p
+
+instance Num Polarity where
+    negate Positive = Negative
+    negate Negative = Positive
+    negate p = p
+
+    Fixed * _ = Fixed
+    _ * Fixed = Fixed
+    Unspecified * _ = Unspecified
+    _ * Unspecified = Unspecified
+    Positive * Positive = Positive
+    Positive * Negative = Negative
+    Negative * Negative = Positive
+    Negative * Positive = Negative
+
+    fromInteger = error "fromInteger not defined on polarity"
+    (+) = error "addition not defined on polarity"
+    abs = error "abs not defined on polarity"
+    signum = error "signum not defined on polarity"
 
 done :: MonadPlus m => m a
 done = mzero
@@ -64,9 +96,9 @@ type M = ErrorT String LFreshM
 beta :: Expr -> MaybeT M Expr
 beta (Var _) = done
 beta (Constant _) = done
-beta (Pi t z) = lunbind z $ \(x, e) ->
-        Pi <$> beta t <*> pure (bind x e)
-    <|> Pi <$> pure t <*> fmap (bind x) (beta e)
+beta (Pi p t z) = lunbind z $ \(x, e) ->
+        Pi p <$> beta t <*> pure (bind x e)
+    <|> Pi p <$> pure t <*> fmap (bind x) (beta e)
 beta (Lambda t z) = lunbind z $ \(x, e) -> do
         Lambda <$> beta t <*> pure (bind x e)
     <|> Lambda <$> pure t <*> fmap (bind x) (beta e)
@@ -101,6 +133,26 @@ relations = Map.fromList
     , rel Box   BoxBox      BoxBox
     ]
 
+polarityOf :: Map (Name Expr) Expr -> Name Expr -> Expr -> M Polarity
+polarityOf _ _ (Constant _) = return Fixed
+polarityOf _ n (Var x) | n == x    = return Positive
+                       | otherwise = return Fixed
+polarityOf m n (App t1 t2) = do
+    k <- typeOf m t1
+    p1 <- polarityOf m n t1
+    p2 <- polarityOf m n t2
+    case k of
+        Pi p _ _ -> return (lub p1 (p * p2))
+        _ -> throwError ("polarityOf: attempting to apply non-pi type")
+polarityOf m n (Lambda t z) = lunbind z $ \(x, e) -> do
+    if Set.member n (fv t)
+        then return Unspecified -- conservative choice
+        else polarityOf (Map.insert x t m) n e
+polarityOf m n (Pi _ t1 z) = lunbind z $ \(x, t2) -> do
+    p1 <- polarityOf m n t1
+    p2 <- polarityOf (Map.insert x t1 m) n t2
+    return (lub (-p1) p2)
+
 typeOf :: Map (Name Expr) Expr -> Expr -> M Expr
 typeOf _ (Constant c) = Constant <$> axiom c
 typeOf m (Var n) = maybe (throwError ("typeOf: unbound variable " ++ show n)) return (Map.lookup n m)
@@ -108,8 +160,8 @@ typeOf m e@(App f a) = do
     tf <- typeOf m f
     ta <- typeOf m a
     case tf of
-        Pi ta' z | ta `aeq` ta' -> lunbind z $ \(x, tb) -> normalizingSubst x a tb
-                 | otherwise -> throwError ("typeOf: ill-typed application '" ++ ppshow e ++ "', expected '" ++ ppshow ta' ++ "' but got '" ++ ppshow ta ++ "'")
+        Pi _ ta' z | ta `aeq` ta' -> lunbind z $ \(x, tb) -> normalizingSubst x a tb
+                   | otherwise -> throwError ("typeOf: ill-typed application '" ++ ppshow e ++ "', expected '" ++ ppshow ta' ++ "' but got '" ++ ppshow ta ++ "'")
         _ -> throwError ("typeOf: attempting to apply non-pi type '" ++ ppshow tf ++ "' in '" ++ ppshow e ++ "'")
 typeOf m (Lambda ta z) = lunbind z $ \(x, b) -> do
     t1 <- typeOf m ta
@@ -119,9 +171,12 @@ typeOf m (Lambda ta z) = lunbind z $ \(x, b) -> do
     case (t1, t2) of
         (Constant s1, Constant s2) -> case Map.lookup (s1, s2) relations of
             Nothing -> throwError "typeOf: lambda relation violation"
-            Just _ -> return (Pi ta (bind x tb))
+            Just _ -> do
+                p <- polarityOf m' x b
+                return (Pi p ta (bind x tb))
         _ -> throwError "typeOf: lambda not sorts"
-typeOf m (Pi ta z) = lunbind z $ \(x, tb) -> do
+typeOf m (Pi _ ta z) = lunbind z $ \(x, tb) -> do
+    -- XXX polarity does not seem to affect the type of pi
     t1 <- typeOf m ta
     t2 <- typeOf (Map.insert x ta m) tb
     case (t1, t2) of
@@ -145,14 +200,26 @@ instance Pretty Constant where
     ppr _ Star = return (PP.text "★")
     ppr _ StarStar = return (PP.text "★★")
 
+instance Pretty Polarity where
+    ppr _ = pprPolarity
+    -- ppr _ = pprNop
+
+pprNop _ = return $ PP.text ""
+pprPolarity Positive = return $ PP.text "⁺"
+pprPolarity Negative = return $ PP.text "⁻"
+pprPolarity Fixed = return $ PP.text "°"
+pprPolarity Unspecified = return $ PP.text "±"
+
+nopEq x y = if x == y then return (PP.text "") else ppr 0 y
+
 instance Pretty Expr where
     ppr _ (Var n) = return (PP.text (show n))
     ppr _ (Constant c) = ppr 0 c
     ppr p (Lambda t z) = pbinds p "λ" t z
-    ppr p (Pi t z) = fmap (prettyParen (p > 0)) . lunbind z $ \(x, e) -> do
+    ppr p (Pi pm t z) = fmap (prettyParen (p > 0)) . lunbind z $ \(x, e) -> do
         if Set.member x (fv e)
-            then pbind "Π" x <$> ppr 0 t <*> ppr 0 e
-            else parr <$> ppr 0 t <*> ppr 0 e
+            then pbind "Π" x <$> nopEq Unspecified pm <*> ppr 0 t <*> ppr 0 e
+            else parr' <$> ppr 0 t <*> nopEq Positive pm <*> ppr 0 e
     ppr p (App e1 e2) = prettyParen (p > 1) <$> ((<+>) <$> ppr 1 e1 <*> ppr 2 e2)
 
 class Pretty p where
@@ -160,18 +227,18 @@ class Pretty p where
 
 prettyParen True = PP.parens
 prettyParen False = id
-parr a b = PP.hang a (-2) (PP.text "→" <+> b)
-pbind b n k e = PP.hang (PP.text b <> PP.parens (PP.text (show n) <+> PP.colon <+> k) <> PP.text ".") 2 e
+parr' a b c = PP.hang a (-2) (PP.text "→" <> b <+> c)
+pbind b n pm k e = PP.hang (PP.text b <> pm <> PP.parens (PP.text (show n) <+> PP.colon <+> k) <> PP.text ".") 2 e
 pbinds p c k b = fmap (prettyParen (p > 0)) . lunbind b $ \(n,t) ->
-    pbind c n <$> ppr 0 k <*> ppr 0 t
+    pbind c n (PP.text "") <$> ppr 0 k <*> ppr 0 t
 
 infixl 9 #
 infixr 1 ~>
 
 lam x t e = Lambda t $ bind (string2Name x) e
-forall x t e = Pi t $ bind (string2Name x) e
+forall x t e = Pi Positive t $ bind (string2Name x) e
 (#) = App
-a ~> b = Pi a $ bind (s2n "_") b
+a ~> b = Pi Positive a $ bind (s2n "_") b
 star = Constant Star
 phi f1 f2 = forall "a" star (f1 # "a" ~> f2 # "a")
 
