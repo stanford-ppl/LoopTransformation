d6d495f59911815cf1a5e854c3a8f2a2a315b6af
Edward Z. Yang <ezyang@mit.edu>
2013-06-05 17:23:55 -0700

    More inference nonsense.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- haskell/pts.hs
+++ haskell/pts.hs
@@ -14,11 +14,13 @@
 import Unbound.LocallyNameless
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
 import qualified Data.Set as Set
 import Control.Monad.Trans.Maybe
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Error
+import Control.Monad.Trans.Writer
 import qualified Text.PrettyPrint as PP
 import Text.PrettyPrint (Doc, (<+>), (<>))
 import Data.String
@@ -49,14 +51,25 @@ data UExpr = UVar (Name UExpr)
            | ULambda (Bind (Name UExpr) UExpr)
     deriving (Show)
 
+data Type = TVar (Name Type)
+          | TArr Type Type
+          | TSkolem (Name Expr)
+          | TApp Type Type
+    deriving (Show, Eq)
+
 -- UNBOUND NONSENSE
 
 $(derive [''Expr, ''Sort, ''Polarity])
 $(derive [''UExpr])
+$(derive [''Type])
 instance Alpha Expr
 instance Alpha Sort
 instance Alpha UExpr
 instance Alpha Polarity
+instance Alpha Type
+instance Subst Type Type where
+    isvar (TVar v) = Just (SubstName v)
+    isvar _ = Nothing
 instance Subst Expr Sort where
 instance Subst Expr Polarity where
 instance Subst Expr Expr where
@@ -100,7 +113,7 @@ instance Num Polarity where
 done :: MonadPlus m => m a
 done = mzero
 
-type M = ErrorT String LFreshM
+type M = ErrorT String (LFreshM)
 
 runM = either error id . runLFreshM . runErrorT
 
@@ -139,12 +152,13 @@ normalizingSubst n e e' = normalize (subst n e e')
 
 rel a b c = ((a, b), c)
 relations = Map.fromList
-    [ rel Star  Star        Star
-    , rel Star  StarStar    StarStar
-    , rel Box   Star        StarStar
-    , rel Box   StarStar    StarStar
-    , rel Box   Box         Box
-    , rel Box   BoxBox      BoxBox
+    -- let α t : *, p : **, * : ☐, **, ☐☐
+    [ rel Star  Star        Star        -- t -> t : *   (functions on monotypes are monotypes)
+    , rel Star  StarStar    StarStar    -- t -> p : *   (quantifiers do not have to be prenex)
+    , rel Box   Star        StarStar    -- ∀α. t : **   (quantifiers cause polytypes)
+    , rel Box   StarStar    StarStar    -- ∀α. p : **   (nested quantifiers are ok)
+    , rel Box   Box         Box         -- * -> * : ☐   (type functions on monotypes are monotypes)
+    , rel Box   BoxBox      BoxBox      -- * -> ** : ☐☐ (type functions on polytypes are polytypes)
     ]
 
 polarityOf :: Map (Name Expr) Expr -> Name Expr -> Expr -> M Polarity
@@ -201,9 +215,7 @@ typeOf m (Pi _ ta z) = lunbind z $ \(x, tb) -> do
 
 tof = runM . typeOf library
 
--- TYPE INFERENCE
-
-erz = runM . erase library
+-- ERASURE
 
 erase :: Map (Name Expr) Expr -> Expr -> M UExpr
 erase m e@(Var n) = do
@@ -233,6 +245,174 @@ erase m (App e1 e2) = do
         _ -> erase m e1
 erase _ (Pi _ _ _) = throwError "erase: pi has no computational content"
 erase _ (Sort _) = throwError "erase: constant has no computational content"
+
+erz = runM . erase library
+
+-- TYPE INFERENCE
+
+{-
+
+What's going on here?
+
+* We have an untyped lambda calculus UExpr. Ordinarily, typing this
+  calculus would be straightforward. (Vanilla Hindley-Milner, and
+  for any unresolved type variables, we just slap quantifiers on the
+  front until it's good.  There is the somewhat delicate issue of
+  let-polymorphism--we then have to muck around with quantifier
+  instantiation--however, we're not including it in our language.)
+
+* However, we have a global context which contains terms which are
+  typed in the full language.  UExpr may refer to these terms, so
+  we need to consider them appropriately.
+
+* In rank-1 polymorphic calculus, this is still not a big deal;
+  all such terms can be converted to prenex form, and then the
+  variables introduced freshly as new things to bind to.
+
+* However, we also have a typing context, which means some of the
+  type variables are pre-bound in the environment, and should not
+  be unified away.  Only variables under the quantifiers are fair
+  game; the other variables are skolem variables.
+
+-}
+
+type GM = ErrorT String (FreshM)
+
+runGM = either error id . runFreshM . runErrorT
+upGM = either throwError return . runLFreshM . runErrorT
+
+--  a local context 'm' (lets us do kind-checking)
+--  a unification variable map (tells us how to translate bound variables)
+--  global context is unnecessary; we can infer if a variable is free if
+--  it's not in the unification variable map
+--  INVARIANT: all types in global context are beta-normalized!
+interpretAsHMType :: Map (Name Expr) Expr -> Map (Name Expr) (Name Type) -> Expr -> GM Type
+interpretAsHMType _ _ (Sort _) = throwError ("tap: cannot interpret sort")
+interpretAsHMType m u (Var n) = case Map.lookup n u of
+    Just t -> return (TVar t)
+    Nothing -> case Map.lookup n m of
+        Just _ -> return (TSkolem n)
+        Nothing -> throwError "tap: unbound variable"
+interpretAsHMType m u (Pi _ ta z) = do
+    (x, tb) <- unbind z
+    t1 <- upGM $ typeOf m ta
+    let m' = Map.insert x ta m
+    t2 <- upGM $ typeOf m' tb
+    let -- since arrows do not introduce new type variables, u is unmodified;
+        -- in fact, lack of dependence states that any further types
+        -- should type-check sans the annotation; we thus omit it and
+        -- expect an unbound variable error if there actually was a
+        -- problem
+        starstar = TArr <$> interpretAsHMType m u ta <*> interpretAsHMType m u tb
+        -- polymorphism! a fresh variable must be added
+        boxstar = do t <- fresh (s2n "t")
+                     interpretAsHMType m' (Map.insert x t u) tb
+    case (t1, t2) of
+        (Sort s1, Sort s2) -> case (s1, s2) of
+            (Star, Star) -> starstar
+            (Star, StarStar) -> starstar
+            (Box, Star) -> boxstar
+            (Box, StarStar) -> boxstar
+            _ -> throwError ("tap: not implemented " ++ ppshow s1 ++ ", " ++ ppshow s2)
+        _ -> throwError "tap: pi not sorts"
+interpretAsHMType _ _ (Lambda _ _) = throwError "tap: illegal unnormalized lambda present in type"
+interpretAsHMType m u (App t1 t2) = do
+    k1 <- upGM $ typeOf m t1
+    s1 <- upGM $ typeOf m k1
+    case s1 of
+        Sort Box -> return ()
+        Sort BoxBox -> return ()
+        _ -> throwError "tap: illegal kind"
+    -- no attempt made at normalization; assumed already normalized, so
+    -- must be an atomic TApp
+    TApp <$> interpretAsHMType m u t1 <*> interpretAsHMType m u t2
+
+constraintsOf :: Map (Name Expr) Expr -> UExpr -> GM ((Expr, Type), [(Type, Type)])
+constraintsOf ctx = runWriterT . f Map.empty
+    where f :: Map (Name UExpr) Type -> UExpr -> WriterT [(Type, Type)] GM (Expr, Type)
+          f m (UVar n) = case Map.lookup n m of
+            Just t -> return (Var (translate n), t)
+            Nothing -> case Map.lookup (translate n) ctx of
+                Just t -> do
+                    -- notice that 'm' is irrelevant for HM types; this
+                    -- is because we have no dependence on terms in the
+                    -- types!
+                    t' <- lift $ interpretAsHMType ctx Map.empty t
+                    return (Var (translate n), t')
+                Nothing -> throwError ("constraintsOf: unbound variable " ++ show n)
+          f m (UApp e1 e2) = do
+            (e1', t1) <- f m e1
+            (e2', t2) <- f m e2
+            t <- fresh (s2n "t")
+            tell [(t1, TArr t2 (TVar t))]
+            return (App e1' e2', (TVar t))
+          f m (ULambda z) = do
+            (x, e) <- unbind z
+            t1 <- fresh (s2n "t")
+            (e', t2) <- f (Map.insert x (TVar t1) m) e
+            return (Lambda (Var (translate t1)) (bind (translate x) e'), TArr (TVar t1) t2)
+
+rmap f x = fmap (\(a,b) -> (a, f b)) x
+solve eq = f eq []
+    where f eq s = case eq of
+            [] -> return s
+            ((t1, t2):eq') | t1 == t2 -> f eq' s
+            ((TVar k, t):eq') | not (Set.member k (fv t)) ->
+                let ts = subst k t
+                in f (map (\(a,b) -> (ts a, ts b)) eq')
+                     ((k,t):rmap ts s)
+            ((t, TVar k):eq') | not (Set.member k (fv t)) ->
+                let ts = subst k t
+                in f (map (\(a,b) -> (ts a, ts b)) eq')
+                     ((k,t):rmap ts s)
+            ((TArr u1 v1, TArr u2 v2):eq') -> f ((u1,u2):(v1,v2):eq') s
+            ((TApp u1 v1, TApp u2 v2):eq') -> f ((u1,u2):(v1,v2):eq') s
+            _ -> throwError "Could not unify"
+
+calculateQuantifiers uv cover =
+    let uv' = Set.difference uv cover
+        vs  = Set.intersection uv cover
+        generate [] f = f
+        -- this is wrong, need to infer kinds
+        generate (x:xs) f = generate xs (\r -> Pi Unspecified (Sort Star) (bind (translate x) (f r)))
+    in (uv', generate (Set.elems vs) id)
+
+translateHMType :: Set (Name Type) -> Type -> GM Expr
+translateHMType _ (TSkolem n) = return (Var n)
+translateHMType uv (TVar n) = do
+    let (uv', qs) = calculateQuantifiers uv (Set.singleton n)
+    unless (Set.null uv') $ throwError "translateHMType: bug! free variables never showed up!"
+    return (qs (Var (translate n)))
+translateHMType uv (TArr t1 t2) = do
+    x <- fresh (s2n "_")
+    let (uv', qs) = calculateQuantifiers uv (fv t1)
+    e1 <- translateHMType uv' t1
+    e2 <- translateHMType uv' t2
+    return (qs (Pi Unspecified e1 (bind x e2)))
+translateHMType uv (TApp t1 t2) = do
+    let (uv', qs) = calculateQuantifiers uv (Set.union (fv t1) (fv t2))
+    e1 <- translateHMType uv' t1
+    e2 <- translateHMType uv' t2
+    return (qs (App e1 e2))
+
+inferType :: Map (Name Expr) Expr -> UExpr -> GM Expr
+inferType ctx e = do
+    ((_, t), eq) <- constraintsOf ctx e
+    subs <- solve eq
+    let t' = substs subs t
+    translateHMType (fv t') t'
+
+infer = runGM . inferType library
+
+{-
+elaborate :: Map (Name Expr) Expr -> UExpr -> GM Expr
+elaborate ctx e = do
+    ((e, _), eq) <- constraintsOf ctx e
+    subs <- solve eq
+    return (substs subs e)
+
+elab = runGM . elaborate library
+-}
 
 -- PRETTY PRINTING
 
